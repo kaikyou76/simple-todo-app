1d69aec0792bde5a03f1c7c855fb51982832be5d
kaikyou76 <kaikyou@gmail.com>
2025-02-12 21:35:12 +0900

    005【handleSubmit関数の開発】

--- src/app/components/TodoForm.tsx
+++ src/app/components/TodoForm.tsx
@@ -1,13 +1,24 @@
 "use client";
-import React from 'react'
+import React, { FormEvent, useRef } from 'react'
 
 const TodoForm = () => {
+    const titleRef = useRef<HTMLInputElement>(null);
+    const contentRef = useRef<HTMLInputElement>(null);
+
+    const handleSubmit = (e:FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const title = titleRef.current!.value;
+        const content = contentRef.current!.value;
+        if(!title || !content)
+            return console.log("タイトル、また内容を入力してください");
+        return console.log(`タイトル: ${title} 内容: ${content}`);
+    }
   return (
-    <form className='flex flex-col w-full'>
+    <form className='flex flex-col w-full' onSubmit={handleSubmit}>
         <label>タイトル</label>
-        <input type="text" className='border-2 rounded-lg p-1' />
+        <input type="text" className='border-2 rounded-lg p-1' ref={titleRef} />
         <label>内容</label>
-        <input type="text" className='border-2 rounded-lg p-1' />
+        <input type="text" className='border-2 rounded-lg p-1' ref={contentRef}/>
         <div className='flex space-x-4 mt-4'>
             <button type='submit' className='bg-black text-white rounded-lg w-full p-1'>
                 登録
