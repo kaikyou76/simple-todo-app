c8748bbf792c508c3c82ad4e39c31eb0ec3ff9bf
kaikyou76 <kaikyou@gmail.com>
2025-04-15 21:27:42 +0900

    D1データベースの作成

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,7 @@
 import { Todo } from "../types/Todo"; 
 //const BASE_URL = "https://spring-frog-3bdf.kai-kyou.workers.dev";
-const BASE_URL = "http://127.0.0.1:8787";//バックエンド
+//const BASE_URL = "http://127.0.0.1:8787";//バックエンド
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export async function createTodo(todo: Todo): Promise<Todo> {
   try {
